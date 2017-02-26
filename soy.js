bc602a86e65cc6c91cb553e3df8352e76a513460
Kevin <kevin.chang.1@stonybrook.edu>
2017-02-25 23:49:14 -0500

    Added death animation

--- soy.js
+++ soy.js
@@ -77,6 +77,7 @@ function create(){
     //add player controlled ship
     ship = game.add.sprite(32, game.world.height - 150, 'ship');
 	ship.health = 512;
+    ship.animations.add('death', [1, 2, 3], 5, true);
     collisionShip = game.add.sprite(ship.x + 21,game.world.height - 140, 'colShip');
     game.physics.arcade.enable(collisionShip);
     collisionShip.visible = true;
@@ -229,10 +230,15 @@ function update(){
     game.physics.arcade.overlap(soyBottles, enemies, damageEnemy,null,this);
     //game.physics.arcade.collide(collisionShip,enemies);
     //damagePlayer(collisionShip,enemies);
-    game.physics.arcade.overlap(collisionShip, enemies, damagePlayer,null,this);
-	game.physics.arcade.overlap(collisionShip, enemyProjectiles, damagePlayer, null, this);
-    collisionShip.x = ship.x+21;
-    collisionShip.y = ship.y+10;
+    if(!isDead){
+        game.physics.arcade.overlap(collisionShip, enemies, damagePlayer,null,this);
+        game.physics.arcade.overlap(collisionShip, enemyProjectiles, damagePlayer, null, this);
+        collisionShip.x = ship.x+21;
+        collisionShip.y = ship.y+10;
+    }else{
+        ship.animations.play('death');
+    }
+    
     if(gameStarted){
         gameplay();
     }
@@ -242,7 +248,6 @@ function damagePlayer(collisionShip,enemy){
 	healthFore.width = game.width *(ship.health / 512);
     enemy.kill();
     if(ship.health <= 3){
-        ship.kill();
         collisionShip.kill();
 		isDead = true;
     }
@@ -309,6 +314,9 @@ function bossMovementHandler(){
 	}, this);
 }
 function controlHandler(){
+    if(isDead){
+        return;
+    }
     //remove this comment after adding animations
     if(cursors.left.isDown){
         ship.body.velocity.x = -300;
@@ -324,8 +332,10 @@ function controlHandler(){
     }
     else{
         // idle
-        ship.animations.stop();
-        ship.frame = 0;
+        if(!isDead){
+            ship.animations.stop();
+            ship.frame = 0;
+        }
     }
 
     if(spaceKey.isDown){
